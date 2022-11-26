fa6b8657374da5ed1e101503192bb716fb6a3668
t.grigorov <t.grigorov99@gmail.com>
2022-11-26 17:32:16 +0100

    Setup File and Folder structure.

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,8 +1,22 @@
-import './styles.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
-  // const name = "Todor";
-  return <h1>React Typescript Webpack Portfolio</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack Portfolio</h1>
+      <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default App;
--- frontend_react/src/index.tsx
+++ frontend_react/src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './styles.scss';
 
 const container = document.getElementById('root');
 const errorContainer = createNoRootElement();
