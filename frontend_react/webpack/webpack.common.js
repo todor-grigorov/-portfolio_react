c9d9113e344b1a0a15a2f2664ba41862e830d0d5
t.grigorov <t.grigorov99@gmail.com>
2022-11-26 13:24:03 +0100

    Change dev server port to 3000

--- frontend_react/webpack/webpack.common.js
+++ frontend_react/webpack/webpack.common.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', 'jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
   },
   module: {
     rules: [
@@ -40,6 +48,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    // new CopyPlugin({ patterns: [{ from: 'public' }] }),
   ],
   stats: 'errors-only',
 };
