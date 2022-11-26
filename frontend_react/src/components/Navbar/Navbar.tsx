563008e83b5d9c9746ded0e4c21688145922335e
t.grigorov <t.grigorov99@gmail.com>
2022-11-26 18:50:03 +0100

    Add assets and configure tsconfig.json to include images.

--- frontend_react/src/components/Navbar/Navbar.tsx
+++ frontend_react/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,14 @@
 import './Navbar.scss';
+import { images } from '../../constants';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt={'logo'} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
