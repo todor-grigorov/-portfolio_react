741c04cf2c59611f0d01e0a7796dd182ca937269
t.grigorov <t.grigorov99@gmail.com>
2022-12-09 13:38:24 +0100

    Commented Testimonials because there are no client testimonials

--- frontend_react/src/container/Testimonial/Testimonial.tsx
+++ frontend_react/src/container/Testimonial/Testimonial.tsx
@@ -1,4 +1,4 @@
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+// import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'; // Uncomment when there is client Testimonial
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { client, urlFor } from '../../client';
@@ -21,7 +21,7 @@ export interface Brand {
 const Testimonial = () => {
   const [brands, setBrands] = useState<Brand[]>([]);
   const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0); // Uncomment when there is client Testimonial
 
   useEffect(() => {
     const brandsQuery = '*[_type == "brands"]';
@@ -36,65 +36,66 @@ const Testimonial = () => {
     });
   }, []);
 
-  const currentTestimonial = testimonials[currentIndex];
-
-  const handleClick = (index: number) => {
-    setCurrentIndex(index);
-  };
+  // const currentTestimonial = testimonials[currentIndex]; // Uncomment when there is client Testimonial
+  //
+  // const handleClick = (index: number) => { // Uncomment when there is client Testimonial
+  //   setCurrentIndex(index);
+  // };
 
   return (
     <>
       {testimonials.length && (
         <>
-          <div className="app__testimonial-item app__flex">
-            <img
-              src={urlFor(currentTestimonial.imgurl).url()}
-              alt="testimonial"
-            />
-            <div className="app__testimonial-content">
-              <p className="p-text">{currentTestimonial.feedback}</p>
-              <div>
-                <h4 className="bold-text">{currentTestimonial.name}</h4>
-                <h5 className="p-text">{currentTestimonial.company}</h5>
-              </div>
-            </div>
-          </div>
-          <div className="app__testimonial-btns app__flex">
-            <div
-              className="app__flex"
-              role={'button'}
-              tabIndex={-1}
-              onClick={() =>
-                handleClick(
-                  currentIndex === 0
-                    ? testimonials.length - 1
-                    : currentIndex - 1
-                )
-              }
-              onKeyDown={() => {
-                console.log('Key pressed');
-              }}
-            >
-              <HiChevronLeft />
-            </div>
-            <div
-              className="app__flex"
-              role={'button'}
-              tabIndex={-1}
-              onClick={() =>
-                handleClick(
-                  currentIndex === testimonials.length - 1
-                    ? 0
-                    : currentIndex + 1
-                )
-              }
-              onKeyDown={() => {
-                console.log('Key pressed');
-              }}
-            >
-              <HiChevronRight />
-            </div>
-          </div>
+          {/*Uncomment when there is client Testimonial*/}
+          {/*<div className="app__testimonial-item app__flex">*/}
+          {/*  <img*/}
+          {/*    src={urlFor(currentTestimonial.imgurl).url()}*/}
+          {/*    alt="testimonial"*/}
+          {/*  />*/}
+          {/*  <div className="app__testimonial-content">*/}
+          {/*    <p className="p-text">{currentTestimonial.feedback}</p>*/}
+          {/*    <div>*/}
+          {/*      <h4 className="bold-text">{currentTestimonial.name}</h4>*/}
+          {/*      <h5 className="p-text">{currentTestimonial.company}</h5>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+          {/*<div className="app__testimonial-btns app__flex">*/}
+          {/*  <div*/}
+          {/*    className="app__flex"*/}
+          {/*    role={'button'}*/}
+          {/*    tabIndex={-1}*/}
+          {/*    onClick={() =>*/}
+          {/*      handleClick(*/}
+          {/*        currentIndex === 0*/}
+          {/*          ? testimonials.length - 1*/}
+          {/*          : currentIndex - 1*/}
+          {/*      )*/}
+          {/*    }*/}
+          {/*    onKeyDown={() => {*/}
+          {/*      console.log('Key pressed');*/}
+          {/*    }}*/}
+          {/*  >*/}
+          {/*    <HiChevronLeft />*/}
+          {/*  </div>*/}
+          {/*  <div*/}
+          {/*    className="app__flex"*/}
+          {/*    role={'button'}*/}
+          {/*    tabIndex={-1}*/}
+          {/*    onClick={() =>*/}
+          {/*      handleClick(*/}
+          {/*        currentIndex === testimonials.length - 1*/}
+          {/*          ? 0*/}
+          {/*          : currentIndex + 1*/}
+          {/*      )*/}
+          {/*    }*/}
+          {/*    onKeyDown={() => {*/}
+          {/*      console.log('Key pressed');*/}
+          {/*    }}*/}
+          {/*  >*/}
+          {/*    <HiChevronRight />*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </>
       )}
 
