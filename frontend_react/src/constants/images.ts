e2a564291bcd2bec0e2db1e2debb44d894b853b5
t.grigorov <t.grigorov99@gmail.com>
2023-01-03 20:02:08 +0100

    add real profile picture

--- frontend_react/src/constants/images.ts
+++ frontend_react/src/constants/images.ts
@@ -23,7 +23,8 @@ import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
-import profile from '../assets/profile.png';
+// import profile from '../assets/profile.png';
+import profile from '../assets/profile-removebg.png';
 import circle from '../assets/circle.svg';
 import logoTest from '../assets/logo-test.png';
 import logo from '../assets/logo.png';
